8fb16a0da28a68b73f76b14295c5073ecb6cf9c5
tadedaba <erbax004@umn.edu>
2024-06-13 17:58:32 -0500

    end of Unit 3.

--- LearningAngular/src/app/app.component.ts
+++ LearningAngular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { OrdersModule } from './orders/orders.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, OrdersModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
