77d89f815e975b9f19eb59da67fbbd3cb17d5c1a
tadedaba <erbax004@umn.edu>
2024-06-18 12:03:56 -0500

    component communication - Inputs

--- LearningAngular/src/app/app.component.ts
+++ LearningAngular/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, OrdersModule],
+  imports: [CommonModule, RouterOutlet, OrdersModule, ProductsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- LearningAngular/src/app/products/products.module.ts
+++ LearningAngular/src/app/products/products.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListComponent } from './products-list/products-list.component';
-import { ProductComponent } from './product/product.component';
-
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 
 @NgModule({
   declarations: [
     ProductsListComponent,
+    ProductDetailComponent,
   ],
   imports: [
     CommonModule,
-    ProductComponent,
   ],
-  exports: [ProductsListComponent],
+  exports: [
+    ProductsListComponent,
+    ProductDetailComponent,
+  ],
 })
 export class ProductsModule { }
