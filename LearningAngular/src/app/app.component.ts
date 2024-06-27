4593fa1f65bc40b440641ed25633664dfd9308b4
tadedaba <erbax004@umn.edu>
2024-06-26 20:59:47 -0500

    no changes

--- LearningAngular/src/app/app.component.ts
+++ LearningAngular/src/app/app.component.ts
@@ -3,11 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, OrdersModule, ProductsModule],
+  imports: [
+    CommonModule, RouterOutlet, OrdersModule, 
+    ProductsModule, MatButtonModule, MatDividerModule,
+    
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
